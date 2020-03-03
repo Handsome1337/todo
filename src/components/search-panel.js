5e6a8f228f763b1869fb1be16f6d2d15a8fc5b80
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-03 19:14:27 +0300

    Начинает стилизацию приложения

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import './search-panel.css';
 
-const SearchPanel = () => <input placeholder="search" />;
+const SearchPanel = () => <input className="search-input form-control" type="text" placeholder="type to search" />;
 
 export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
+import ItemStatusFilter from './components/item-status-filter';
 import TodoList from './components/todo-list';
+import './index.css';
 
 const App = () => {
   const todoData = [
@@ -12,9 +14,12 @@ const App = () => {
   ];
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
       <TodoList todos={todoData}/>
     </div>
   );
