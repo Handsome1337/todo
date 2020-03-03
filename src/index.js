fb58214209d78a4674b016bc3db2f706fde8de00
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-03 13:15:20 +0300

    Создаёт первые React компоненты

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My To-Do List</h1>
-    <input placeholder="search" />
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Build Awesome App</li>
     </ul>
-  </div>
-);
+  );
+};
 
-ReactDOM.render(el, document.getElementById('root'));
+const AppHeader = () => <h1>My To-Do List</h1>;
+
+const SearchPanel = () => <input placeholder="search" />;
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
