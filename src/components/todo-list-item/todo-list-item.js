0df9ef9ac6e1d67f7f5b72234345738429581479
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-04 12:29:20 +0300

    Переписывает динамические компоненты на классы

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
+export default class TodoListItem extends Component {
+  render() {
+    const { label, important = false } = this.props;
 
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>{ label }</span>
-      <button className="btn btn-outline-success btn-sm float-right" type="button">
-        <i className="fa fa-exclamation" />
-      </button>
-      <button className="btn btn-outline-danger btn-sm float-right" type="button">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
 
-export default TodoListItem;
+    return (
+      <span className="todo-list-item">
+        <span className="todo-list-item-label" style={style}>{ label }</span>
+        <button className="btn btn-outline-success btn-sm float-right" type="button">
+          <i className="fa fa-exclamation" />
+        </button>
+        <button className="btn btn-outline-danger btn-sm float-right" type="button">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  }
+}
