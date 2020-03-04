cdfa7a40f444314ca2565676ef4bcf51a5676dcc
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-04 14:08:12 +0300

    Добавляет возможность пометить задачу выполненной/важной с помощью состояния (state) React компонента

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,18 +2,39 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-  render() {
-    const { label, important = false } = this.props;
+  constructor() {
+    super();
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
+    this.state = {
+      done: false,
+      important: false
     };
 
+    this.onLabelClick = () => {
+      this.setState(({done}) => ({done: !done}));
+    };
+
+    this.onMarkImportantClick = () => {
+      this.setState(({important}) => ({important: !important}));
+    };
+  }
+
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+    if (important) {
+      classNames += ' important';
+    }
+
     return (
-      <span className="todo-list-item">
-        <span className="todo-list-item-label" style={style}>{ label }</span>
-        <button className="btn btn-outline-success btn-sm float-right" type="button">
+      <span className={classNames}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>{ label }</span>
+        <button className="btn btn-outline-success btn-sm float-right" type="button" onClick={this.onMarkImportantClick}>
           <i className="fa fa-exclamation" />
         </button>
         <button className="btn btn-outline-danger btn-sm float-right" type="button">
