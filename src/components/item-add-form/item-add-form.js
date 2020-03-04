bd4741b1ce37d602aef5d6d7cae57d7c1bf7625f
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-04 21:52:31 +0300

    Реализует форму добавления задач

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
-  render() {
-    const { onItemAdded } = this.props;
+  constructor() {
+    super();
 
+    this.state = {
+      label: ''
+    };
+
+    this.onLabelChange = (e) => {
+      this.setState({
+        label: e.target.value
+      });
+    };
+
+    this.onSubmit = (e) => {
+      e.preventDefault();
+      e.currentTarget.reset();
+      this.props.onItemAdded(this.state.label);
+    };
+  }
+  render() {
     return (
-      <div className="item-add-form">
-        <button className="btn btn-outline-secondary" onClick={() => onItemAdded('New To-Do task')}>Add Item</button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+        <input className="form-control" type="text" placeholder="What needs to be done" onChange={this.onLabelChange} />
+        <button className="btn btn-outline-secondary text-nowrap">Add Item</button>
+      </form>
     );
   }
 };
