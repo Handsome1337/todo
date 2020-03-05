ecb7ef04b2103480a946dfb8da87cab8752ae97d
Ilya Volkov <volkov-ilyha@ya.ru>
2020-03-05 13:56:48 +0300

    Финальные правки

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './todo-list-item.css';
 
-export default class TodoListItem extends Component {
-  render() {
-    const { label, onDeleted, onToggleDone, onToggleImportant, done, important } = this.props;
-
-    let classNames = 'todo-list-item';
-    if (done) {
-      classNames += ' done';
-    }
-    if (important) {
-      classNames += ' important';
-    }
-
-    return (
-      <span className={classNames}>
-        <span className="todo-list-item-label" onClick={onToggleDone}>{ label }</span>
-        <button className="btn btn-outline-success btn-sm float-right" type="button" onClick={onToggleImportant}>
-          <i className="fa fa-exclamation" />
-        </button>
-        <button className="btn btn-outline-danger btn-sm float-right" type="button" onClick={onDeleted}>
-          <i className="fa fa-trash-o" />
-        </button>
-      </span>
-    );
+const TodoListItem = ({ label, onDeleted, onToggleDone, onToggleImportant, done, important }) => {
+  let classNames = 'todo-list-item';
+  if (done) {
+    classNames += ' done';
   }
-}
+  if (important) {
+    classNames += ' important';
+  }
+
+  return (
+    <span className={classNames}>
+      <span className="todo-list-item-label" onClick={onToggleDone}>{ label }</span>
+      <button className="btn btn-outline-success btn-sm float-right" type="button" onClick={onToggleImportant}>
+        <i className="fa fa-exclamation" />
+      </button>
+      <button className="btn btn-outline-danger btn-sm float-right" type="button" onClick={onDeleted}>
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
+};
+
+export default TodoListItem;
